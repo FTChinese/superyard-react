040caf06c1dc8b28bd6890fc89020afd7e96771b
weiguo.ni <neefrankie@gmail.com>
2021-11-26 18:05:39 +0800

    feat: Product component

---
+++ src/features/paywall/Product.tsx
@@ -0,0 +1,61 @@
+import { Link } from 'react-router-dom';
+import { PaywallProduct } from '../../data/paywall';
+import { Price } from '../../data/price';
+import { formatPrice } from '../../utils/format-price';
+
+function TextList(
+  props: {
+    text: string;
+  }
+) {
+  return (
+    <ul>
+      {
+        props.text.split('\n').map((line, index) => (
+          <li key={index}>{line}</li>
+        ))
+      }
+    </ul>
+  );
+}
+
+function PriceButton(
+  props: {
+    price: Price;
+  }
+) {
+  return (
+    <div className="d-grid">
+      <button className="btn btn-primary">
+        {formatPrice(props.price)}
+      </button>
+    </div>
+  );
+}
+
+export function ProductCard(
+  props: {
+    product: PaywallProduct;
+  }
+) {
+  return (
+    <div className="card">
+      <div className="card-header text-end">
+        <Link to={`products/${props.product.id}`}>
+          Edit
+        </Link>
+      </div>
+      <div className="card-body">
+        <h3 className="card-title text-center mb-3 pb-3">
+          {props.product.heading}
+        </h3>
+        {
+          props.product.prices.map((p, i) => (
+            <PriceButton key={i} price={p} />
+          ))
+        }
+        <TextList text={props.product.description}/>
+      </div>
+    </div>
+  );
+}
