786479567906a23e36c539f87584274f78542e4d
weiguo.ni <neefrankie@gmail.com>
2021-11-30 16:48:00 +0800

    refactor: handle price creation and update

--- src/features/paywall/PriceList.tsx
+++ src/features/paywall/PriceList.tsx
@@ -1,83 +1,142 @@
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { PaywallPrice } from '../../data/paywall';
 import { formatPrice } from '../../utils/format-price';
 import { ActiveBadge, ModeBadge } from './Badge';
 import { DiscountList } from './DiscountList';
+import { useState } from 'react';
+import { PriceFormDialog } from './PriceFormDialog';
+import { CMSPassport } from '../../data/cms-account';
+
+export type OnPriceUpdated = (price: PaywallPrice) => void;
 
 export function PriceList(
   props: {
+    passport: CMSPassport;
     prices: PaywallPrice[];
+    onUpdated: OnPriceUpdated;
   }
 ) {
   return (
     <>
       {
-        props.prices.map(price => <PriceDetails key={price.id} price={price}/>)
+        props.prices.map(price =>
+          <PriceListItem
+            key={price.id}
+            passport={props.passport}
+            price={price}
+            onUpdated={props.onUpdated}
+          />
+        )
       }
     </>
   )
 }
 
-export function PriceDetails(
+function PriceListItem(
+  props: {
+    passport: CMSPassport;
+    price: PaywallPrice;
+    onUpdated: OnPriceUpdated;
+  }
+) {
+
+  const [ show, setShow ] = useState(false);
+
+  return (
+    <div className="mb-3">
+      <Card>
+        <Card.Header className="d-flex justify-content-between">
+          <div>
+            <span className="me-2">
+              {formatPrice(props.price)}
+            </span>
+            {
+              props.price.active &&
+              <ActiveBadge active={true}/>
+            }
+          </div>
+
+          <ButtonGroup
+            size="sm"
+          >
+            {
+              !props.price.active &&
+              <Button
+                variant="outline-primary"
+                size="sm"
+              >
+                Activate
+              </Button>
+            }
+            <Button
+              variant="primary"
+              size="sm"
+              onClick={() => setShow(true)}
+            >
+              Edit
+            </Button>
+          </ButtonGroup>
+        </Card.Header>
+        <Card.Body>
+          <PriceContent
+            price={props.price}
+          />
+        </Card.Body>
+      </Card>
+
+      <DiscountList
+        offers={props.price.offers}
+      />
+      <PriceFormDialog
+        passport={props.passport}
+        show={show}
+        onHide={() => setShow(false)}
+        onUpserted={props.onUpdated}
+        price={props.price}
+      />
+    </div>
+  );
+}
+
+function PriceContent(
   props: {
     price: PaywallPrice;
   }
 ) {
   return (
-    <div className="mb-3">
-      <Card>
-        <Card.Header className="d-flex justify-content-between">
-          <span>{formatPrice(props.price)}</span>
-          {
-            props.price.active ?
-            <ActiveBadge active={true}/> :
-            <button className="btn btn-link btn-sm">Activate</button>
-          }
-        </Card.Header>
-        <Card.Body>
-          <table className="table table-borderless">
-            <tbody>
-              <tr>
-                <th>ID</th>
-                <td>{props.price.id}</td>
-              </tr>
-              <tr>
-                <th>Tier</th>
-                <td>{props.price.tier}</td>
-              </tr>
-              <tr>
-                <th>Nickname</th>
-                <td>{props.price.nickname}</td>
-              </tr>
-              <tr>
-                <th>Description</th>
-                <td>{props.price.description}</td>
-              </tr>
-              <tr>
-                <th>Mode</th>
-                <td><ModeBadge live={props.price.liveMode}/></td>
-              </tr>
-              <tr>
-                <th>Created</th>
-                <td>at {props.price.createdUtc} by {props.price.createdBy}</td>
-              </tr>
-            </tbody>
-          </table>
-        </Card.Body>
-      </Card>
-
-      <Card>
-        <Card.Header className="d-flex justify-content-between">
-          <span>Discount list</span>
-          <div className="btn-group">
-            <button className="btn btn-outline-primary btn-sm">Refresh</button>
-            <button className="btn btn-primary btn-sm">New</button>
-          </div>
-        </Card.Header>
-        <DiscountList offers={props.price.offers} />
-      </Card>
-    </div>
+    <table className="table table-borderless">
+      <tbody>
+        <tr>
+          <th>ID</th>
+          <td>{props.price.id}</td>
+        </tr>
+        <tr>
+          <th>Tier</th>
+          <td>{props.price.tier}</td>
+        </tr>
+        <tr>
+          <th>Nickname</th>
+          <td>{props.price.nickname}</td>
+        </tr>
+        <tr>
+          <th>Description</th>
+          <td>{props.price.description}</td>
+        </tr>
+        <tr>
+          <th>Stripe Price ID</th>
+          <td>{props.price.stripePriceId}</td>
+        </tr>
+        <tr>
+          <th>Mode</th>
+          <td><ModeBadge live={props.price.liveMode}/></td>
+        </tr>
+        <tr>
+          <th>Created</th>
+          <td>at {props.price.createdUtc} by {props.price.createdBy}</td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
-
-
--- src/pages/paywall/ProductDetailPage.tsx
+++ src/pages/paywall/ProductDetailPage.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
 import { useParams } from 'react-router';
 import { ErrorBoudary } from '../../components/ErrorBoundary';
 import { LoadingSpinner } from '../../components/progress/LoadingSpinner';
 import { Unauthorized } from '../../components/routes/Unauthorized';
 import { CMSPassport } from '../../data/cms-account';
 import { PaywallPrice, Product } from '../../data/paywall';
-import { PriceList } from '../../features/paywall/PriceList';
+import { Price } from '../../data/price';
+import { PriceFormDialog } from '../../features/paywall/PriceFormDialog';
+import { OnPriceUpdated, PriceList } from '../../features/paywall/PriceList';
 import { ProductDetails } from '../../features/paywall/ProductDetails';
 import { listPriceOfProduct, loadProduct } from '../../repository/paywall';
 import { ResponseError } from '../../repository/response-error';
@@ -15,6 +18,7 @@ import { useLiveState } from '../../store/useLiveState';
 export function ProductDetailPage() {
   const { productId } = useParams<'productId'>();
   const { passport } = useAuthContext();
+  const [ product, setProduct ] = useState<Product>();
 
   if (!productId) {
     return <div>Product id missing in url!</div>;
@@ -30,12 +34,14 @@ export function ProductDetailPage() {
         <LoadProduct
           passport={passport}
           productId={productId}
+          onLoaded={setProduct}
         />
       </section>
       <section>
         <LoadPrices
           passport={passport}
           productId={productId}
+          product={product}
         />
       </section>
     </>
@@ -46,6 +52,7 @@ function LoadProduct(
   props: {
     passport: CMSPassport;
     productId: string;
+    onLoaded: (product: Product) => void;
   }
 ) {
   const [ err, setErr ] = useState('');
@@ -63,6 +70,7 @@ function LoadProduct(
       .then(product => {
         setLoading(false);
         setProduct(product);
+        props.onLoaded(product);
       })
       .catch((err: ResponseError) => {
         setLoading(false);
@@ -91,14 +99,15 @@ function LoadPrices(
   props: {
     passport: CMSPassport,
     productId: string;
+    product?: Product; // Passed from sibling component after product loaded.
   }
 ) {
   const [ err, setErr ] = useState('');
   const [ loading, setLoading ] = useState(true);
-
   const { live } = useLiveState();
+  const [ show, setShow ] = useState(false);
 
-  const [ prices, setPrices ] = useState<PaywallPrice[]>();
+  const [ prices, setPrices ] = useState<PaywallPrice[]>([]);
 
   useEffect(() => {
     listPriceOfProduct(
@@ -115,15 +124,52 @@ function LoadPrices(
       });
     }, [live]);
 
+  const handleCreate = (price: Price) => {
+    setPrices([
+      {
+        ...price,
+        offers: [],
+      },
+      ...prices
+    ])
+  }
+
+  const handleUpdate: OnPriceUpdated = (price: PaywallPrice) => {
+    setPrices(prices.map(p => {
+      if (p.id === price.id) {
+        return price;
+      }
+
+      return p;
+    }));
+  }
+
   return (
     <ErrorBoudary errMsg={err}>
       <LoadingSpinner loading={loading}>
         <>
           <h4 className="d-flex justify-content-between">
             <span>Price List</span>
-            <button className="btn btn-primary">New Price</button>
+            { props.product &&
+              <Button
+                onClick={() => setShow(true)}
+              >
+                New Price
+              </Button>
+            }
           </h4>
-          {prices && <PriceList prices={prices} />}
+          <PriceList
+            passport={props.passport}
+            prices={prices}
+            onUpdated={handleUpdate}
+          />
+          <PriceFormDialog
+            passport={props.passport}
+            show={show}
+            onHide={() => setShow(false)}
+            onUpserted={handleCreate}
+            product={props.product}
+          />
         </>
       </LoadingSpinner>
     </ErrorBoudary>
