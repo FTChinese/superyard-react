da88b5d971a8759b9cd6313ac6507c5fc06b15cd
weiguo.ni <neefrankie@gmail.com>
2021-12-01 18:37:21 +0800

    feat: components

---
+++ src/features/paywall/PriceContent.tsx
@@ -0,0 +1,44 @@
+import { Price } from '../../data/price';
+import { ModeBadge } from './Badge';
+
+export function PriceContent(
+  props: {
+    price: Price;
+  }
+) {
+  return (
+    <table className="table table-borderless caption-top">
+      <caption>Price Details</caption>
+      <tbody>
+        <tr>
+          <th>ID</th>
+          <td>{props.price.id}</td>
+        </tr>
+        <tr>
+          <th>Tier</th>
+          <td>{props.price.tier}</td>
+        </tr>
+        <tr>
+          <th>Nickname</th>
+          <td>{props.price.nickname}</td>
+        </tr>
+        <tr>
+          <th>Description</th>
+          <td>{props.price.description}</td>
+        </tr>
+        <tr>
+          <th>Stripe Price ID</th>
+          <td>{props.price.stripePriceId}</td>
+        </tr>
+        <tr>
+          <th>Mode</th>
+          <td><ModeBadge live={props.price.liveMode}/></td>
+        </tr>
+        <tr>
+          <th>Created</th>
+          <td>at {props.price.createdUtc} by {props.price.createdBy}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
