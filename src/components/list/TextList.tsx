819c3ac0499ad1ee52a23ef00e50ae624011325a
weiguo.ni <neefrankie@gmail.com>
2021-12-01 18:35:05 +0800

    feat: ListLines

--- src/components/list/TextList.tsx
+++ src/components/list/TextList.tsx
@@ -1,3 +1,6 @@
+/**
+ * @description Display a line-separated string in a unordered list.
+ */
 export function TextList(
   props: {
     text: string;
@@ -7,6 +10,25 @@ export function TextList(
     <ul>
       {
         props.text.split('\n').map((line, index) => (
+          <li key={index}>{line}</li>
+        ))
+      }
+    </ul>
+  );
+}
+
+/**
+ * @description Display an array of string in unordered list.
+ */
+export function ListLines(
+  props: {
+    lines: string[];
+  }
+) {
+  return (
+    <ul>
+      {
+        props.lines.map((line, index) => (
           <li key={index}>{line}</li>
         ))
       }
