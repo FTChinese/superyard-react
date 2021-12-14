bf0524dcee8d230a36a89f40e01e881291a1859a
weiguo.ni <neefrankie@gmail.com>
2021-12-14 14:16:07 +0800

    feat: OnPriceUpserted

--- src/features/paywall/callbacks.ts
+++ src/features/paywall/callbacks.ts
@@ -1,4 +1,7 @@
 import { PaywallPrice, Product } from '../../data/paywall';
+import { Price } from '../../data/price';
 
-export type OnPriceUpserted = (price: PaywallPrice) => void;
 export type OnProductUpserted = (product: Product) => void;
+export type OnPriceUpserted = (price: Price) => void;
+export type OnPaywallPriceUpserted = (price: PaywallPrice) => void;
+
