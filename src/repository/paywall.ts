e71e42bf1e793f87659ce6025f6be22bd3a4f6c1
weiguo.ni <neefrankie@gmail.com>
2021-12-14 11:27:16 +0800

    feat: attach and drop intro

--- src/repository/paywall.ts
+++ src/repository/paywall.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
-import { BannerParams, Paywall, PaywallDoc, PaywallPrice, Product, UpdateProductParams, PromoParams, NewProductParams, RebuiltResult, StripeRawPrice } from '../data/paywall';
+import { BannerParams, Paywall, PaywallDoc, PaywallPrice, Product, UpdateProductParams, PromoParams, NewProductParams, RebuiltResult, StripeRawPrice, AttachIntroParams } from '../data/paywall';
 import { Discount, DiscountParams, Price, NewPriceParams, UpdatePriceParams } from '../data/price';
 import { endpoint } from './endpoint';
 import { buildReqConfig, ReqConfig } from './ReqConfig';
@@ -29,21 +29,21 @@ export function saveBanner(body: BannerParams, config: ReqConfig): Promise<Paywa
 
 export function savePromo(body: PromoParams, config: ReqConfig): Promise<PaywallDoc> {
   return axios.post<PaywallDoc, AxiosResponse<PaywallDoc>, BannerParams>(
-    endpoint.promo,
-    body,
-    buildReqConfig(config)
-  )
-  .then(resp => resp.data)
-  .catch(error => Promise.reject(ResponseError.newInstance(error)));
+      endpoint.promo,
+      body,
+      buildReqConfig(config)
+    )
+    .then(resp => resp.data)
+    .catch(error => Promise.reject(ResponseError.newInstance(error)));
 }
 
 export function dropPromo(config: ReqConfig): Promise<PaywallDoc> {
   return axios.delete<PaywallDoc, AxiosResponse<PaywallDoc>, BannerParams>(
-    endpoint.promo,
-    buildReqConfig(config)
-  )
-  .then(resp => resp.data)
-  .catch(error => Promise.reject(ResponseError.newInstance(error)));
+      endpoint.promo,
+      buildReqConfig(config)
+    )
+    .then(resp => resp.data)
+    .catch(error => Promise.reject(ResponseError.newInstance(error)));
 }
 
 export function createProduct(body: NewProductParams, config: ReqConfig): Promise<Product> {
@@ -67,31 +67,50 @@ export function listProduct(config: ReqConfig): Promise<Product[]> {
 
 export function loadProduct(id: string, config: ReqConfig): Promise<Product> {
   return axios.get<Product>(
-    endpoint.productOf(id),
-    buildReqConfig(config),
-  )
-  .then(resp => resp.data)
-  .catch(error => Promise.reject(ResponseError.newInstance(error)));
+      endpoint.productOf(id),
+      buildReqConfig(config),
+    )
+    .then(resp => resp.data)
+    .catch(error => Promise.reject(ResponseError.newInstance(error)));
 }
 
 export function activateProduct(id: string, config: ReqConfig): Promise<Product> {
   return axios.post<Product>(
-    endpoint.productOf(id),
-    undefined,
-    buildReqConfig(config),
-  )
-  .then(resp => resp.data)
-  .catch(error => Promise.reject(ResponseError.newInstance(error)));
+      endpoint.productOf(id),
+      undefined,
+      buildReqConfig(config),
+    )
+    .then(resp => resp.data)
+    .catch(error => Promise.reject(ResponseError.newInstance(error)));
 }
 
 export function updateProduct(id: string, body: UpdateProductParams, config: ReqConfig): Promise<Product> {
   return axios.patch<Product>(
-    endpoint.productOf(id),
-    body,
-    buildReqConfig(config),
-  )
-  .then(resp => resp.data)
-  .catch(error => Promise.reject(ResponseError.newInstance(error)));
+      endpoint.productOf(id),
+      body,
+      buildReqConfig(config),
+    )
+    .then(resp => resp.data)
+    .catch(error => Promise.reject(ResponseError.newInstance(error)));
+}
+
+export function attachIntroPrice(prodId: string, body: AttachIntroParams, config: ReqConfig): Promise<Product> {
+  return axios.patch<Product>(
+      endpoint.introForProductOf(prodId),
+      body,
+      buildReqConfig(config),
+    )
+    .then(resp => resp.data)
+    .catch(error => Promise.reject(ResponseError.newInstance(error)));
+}
+
+export function dropIntroPrice(prodId: string, config: ReqConfig): Promise<Product> {
+  return axios.delete<Product>(
+      endpoint.introForProductOf(prodId),
+      buildReqConfig(config),
+    )
+    .then(resp => resp.data)
+    .catch(error => Promise.reject(ResponseError.newInstance(error)));
 }
 
 export function createPrice(body: NewPriceParams, config: ReqConfig): Promise<Price> {
@@ -145,31 +164,31 @@ export function archivePrice(id: string, config: ReqConfig): Promise<PaywallPric
 
 export function refreshPriceOffers(id: string, config: ReqConfig): Promise<PaywallPrice> {
   return axios.patch<PaywallPrice>(
-    endpoint.offerOfPrice(id),
-    undefined,
-    buildReqConfig(config),
-  )
-  .then(resp => resp.data)
-  .catch(error => Promise.reject(ResponseError.newInstance(error)));
+      endpoint.offerOfPrice(id),
+      undefined,
+      buildReqConfig(config),
+    )
+    .then(resp => resp.data)
+    .catch(error => Promise.reject(ResponseError.newInstance(error)));
 }
 
 export function createOffer(body: DiscountParams, config: ReqConfig): Promise<Discount> {
   return axios.post<Discount>(
-    endpoint.discount,
-    body,
-    buildReqConfig(config),
-  )
-  .then(resp => resp.data)
-  .catch(error => Promise.reject(ResponseError.newInstance(error)));
+      endpoint.discount,
+      body,
+      buildReqConfig(config),
+    )
+    .then(resp => resp.data)
+    .catch(error => Promise.reject(ResponseError.newInstance(error)));
 }
 
 export function dropOffer(id: string, config: ReqConfig): Promise<PaywallPrice> {
   return axios.delete<PaywallPrice>(
-    endpoint.discountOf(id),
-    buildReqConfig(config),
-  )
-  .then(resp => resp.data)
-  .catch(error => Promise.reject(ResponseError.newInstance(error)));
+      endpoint.discountOf(id),
+      buildReqConfig(config),
+    )
+    .then(resp => resp.data)
+    .catch(error => Promise.reject(ResponseError.newInstance(error)));
 }
 
 
