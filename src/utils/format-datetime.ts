22190f9d90f6eb247bbca02c0f54b4fc9ebdae90
weiguo.ni <neefrankie@gmail.com>
2021-12-16 16:18:07 +0800

    refactor

--- src/utils/format-datetime.ts
+++ src/utils/format-datetime.ts
@@ -1,23 +1,8 @@
 import { format, parseISO } from 'date-fns';
-import { isZeroYMD, YearMonthDay } from '../data/period';
 
-export function formatISODateTime(dateString: string): string {
+export function localizeISO(dateString: string): string {
   return format(parseISO(dateString), 'yyyy年M月d日H时m分s秒')
 }
 
-export function formatYearMonthDay(ymd: YearMonthDay): string {
 
-  const str: string[] = [];
 
-  str.push(`${ymd.years} ${pluralize('year', ymd.years > 1)}`);
-
-  str.push(`${ymd.months} ${pluralize('month', ymd.months > 1)}`);
-
-  str.push(`${ymd.days} ${pluralize('day', ymd.days > 1)}`);
-
-  return str.join(', ');
-}
-
-function pluralize(word: string, plural: boolean): string {
-  return plural ? `${word}s` : word
-}
