443733e04394dac29e1aa8eac26ba9ed9b543389
weiguo.ni <neefrankie@gmail.com>
2021-12-16 16:14:23 +0800

    removed

--- src/data/period.ts
+++ src/data/period.ts
@@ -1,22 +1,9 @@
-import { padSeconds } from '../utils/time-formatter';
+import { pluralize } from '../utils/pluralize';
 
 export type ISOPeriod = {
   startUtc: string; // ISO
   endUtc: string;
 };
-
-export type DateTime = {
-  date: string;
-  time: string;
-}
-
-export function concatDateTime(dt: DateTime, zone: string): string {
-  if (!dt.date  || !dt.time || !zone) {
-    return '';
-  }
-
-  return `${dt.date}T${padSeconds(dt.time)}${zone}`;
-}
 
 export type YearMonthDay = {
   years: number;
@@ -26,4 +13,24 @@ export type YearMonthDay = {
 
 export function isZeroYMD(ymd: YearMonthDay): boolean {
   return ymd.years === 0 && ymd.months === 0 && ymd.days === 0;
+}
+
+export function ymdZero(): YearMonthDay {
+  return {
+    years: 0,
+    months: 0,
+    days: 0,
+  };
+}
+
+export function formatYMD(ymd: YearMonthDay): string {
+  const str: string[] = [];
+
+  str.push(`${ymd.years} ${pluralize('year', ymd.years > 1)}`);
+
+  str.push(`${ymd.months} ${pluralize('month', ymd.months > 1)}`);
+
+  str.push(`${ymd.days} ${pluralize('day', ymd.days > 1)}`);
+
+  return str.join(', ');
 }
