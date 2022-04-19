696bb3de1e2425012dd8da1211c92de6f70d9a38
weiguo.ni <neefrankie@gmail.com>
2022-04-19 17:37:29 +0800

    feat: coupon dialog

---
+++ src/features/stripe/CouponFromDialog.tsx
@@ -0,0 +1,29 @@
+import Modal from 'react-bootstrap/Modal';
+import { ModeBadge } from '../../components/text/Badge';
+
+export function CouponFormDialog(
+  props: {
+    show: boolean,
+    onHide: () => void;
+    live: boolean;
+  }
+) {
+  return (
+    <Modal
+      show={props.show}
+      fullscreen={true}
+      onHide={props.onHide}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title className="me-3">
+          Create Coupon
+        </Modal.Title>
+        <ModeBadge live={props.live} />
+      </Modal.Header>
+
+      <Modal.Body>
+
+      </Modal.Body>
+    </Modal>
+  );
+}
