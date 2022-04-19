cdabb78e7130dec3638674c6b47a23894708df9c
weiguo.ni <neefrankie@gmail.com>
2022-04-19 13:20:41 +0800

    feat: add refresh field

--- src/repository/ReqConfig.ts
+++ src/repository/ReqConfig.ts
@@ -4,10 +4,15 @@ import { authHeader } from '../data/cms-account';
 export type ReqConfig  = {
   live: boolean; // Live mode or sandbox mode
   token: string; // Json web token set in header.
+  refresh?: boolean
 }
 
 export function buildReqConfig(c: ReqConfig, params: URLSearchParams = new URLSearchParams()): AxiosRequestConfig {
   params.set('live', `${c.live}`);
+
+  if (c.refresh) {
+    params.set('refresh', `${c.refresh}`);
+  }
 
   return {
     headers: authHeader(c.token),
