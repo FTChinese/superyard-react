a939e8edc0470f03023402821c643b13d603f8f6
weiguo.ni <neefrankie@gmail.com>
2022-04-20 16:57:35 +0800

    feat: build coupn form

--- src/features/stripe/CouponForm.tsx
+++ src/features/stripe/CouponForm.tsx
@@ -1,13 +1,74 @@
-import { DateTimeParts } from '../../data/datetime-parts';
+import { Form, Formik, FormikHelpers } from 'formik';
+import * as Yup from 'yup';
+import { DateTimeInput } from '../../components/controls/DateTimeInput';
+import { FormikSubmitButton } from '../../components/controls/FormikSubmitButton';
+import { TextInput } from '../../components/controls/TextInput';
+import { TimezoneBadge } from '../../components/text/Badge';
+import { concatDateTimePartsISO, DateTimeParts, defaultDateTimeParts } from '../../data/datetime-parts';
+import { invalidMessages } from '../../data/form-value';
+import { CouponParams } from '../../data/stripe-price';
+import { currentZone } from '../../utils/time-format';
 
 export type CouponFormVal = {
-  priceId: string;
-  startUtc: DateTimeParts;
-  endUtc: DateTimeParts;
+  couponId: string;
+  start: DateTimeParts;
+  end: DateTimeParts;
 };
 
+export function buildCouponParams(priceId: string, v: CouponFormVal): CouponParams {
+  const zone = currentZone();
+
+  return {
+    priceId: priceId,
+    startUtc: concatDateTimePartsISO(v.start, zone),
+    endUtc: concatDateTimePartsISO(v.end, zone),
+  }
+}
+
 export function CouponForm(
-
+  props: {
+    onSubmit: (
+      values: CouponFormVal,
+      formikHelpers: FormikHelpers<CouponFormVal>
+    ) => void | Promise<any>;
+  }
 ) {
+  return (
+    <Formik<CouponFormVal>
+      initialValues={{
+        couponId: '',
+        start: defaultDateTimeParts(),
+        end: defaultDateTimeParts(),
+      }}
+      validationSchema={Yup.object({
+        couponId: Yup.string()
+          .trim()
+          .required(invalidMessages.required)
+      })}
+      onSubmit={props.onSubmit}
+    >
+      <Form>
+        <TextInput
+          label="Coupon ID"
+          name="couponId"
+          type="text"
+          desc="Copy & Paste a coupon id from Stripe dashboard"
+        />
+        <DateTimeInput
+          title="Promotion Start"
+          namePrefix="start"
+        />
+        <DateTimeInput
+          title="Promotion End"
+          namePrefix="end"
+        />
+        <TimezoneBadge/>
 
+        <FormikSubmitButton
+          text="Save"
+          wrapped="block"
+        />
+      </Form>
+    </Formik>
+  );
 }
--- src/features/stripe/CouponFormDialog.tsx
+++ src/features/stripe/CouponFormDialog.tsx
@@ -1,14 +1,48 @@
+import { FormikHelpers } from 'formik';
+import { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import { FullscreenSingleCol } from '../../components/layout/FullscreenSingleCol';
 import { ModeBadge } from '../../components/text/Badge';
+import { ErrorAlert } from '../../components/text/ErrorAlert';
+import { PriceHighlight } from '../../components/text/PriceHighlight';
+import { CMSPassport } from '../../data/cms-account';
+import { StripeCoupon, StripePrice, stripePriceFormat } from '../../data/stripe-price';
+import { updateCoupon } from '../../repository/paywall';
+import { ResponseError } from '../../repository/response-error';
+import { buildCouponParams, CouponForm, CouponFormVal } from './CouponForm';
 
 export function CouponFormDialog(
   props: {
+    passport: CMSPassport,
+    live: boolean;
+    price: StripePrice;
     show: boolean,
     onHide: () => void;
-    live: boolean;
+    onCreated: (c: StripeCoupon) => void;
   }
 ) {
+
+  const [ err, setErr ] = useState('');
+
+  const handleSubmit = (values: CouponFormVal, helpers: FormikHelpers<CouponFormVal>) => {
+    helpers.setSubmitting(true);
+
+    updateCoupon(
+      values.couponId,
+      buildCouponParams(props.price.id, values),
+      {
+        live: props.live,
+        token: props.passport.token
+      }
+    )
+    .then(c => {
+      props.onCreated(c);
+    })
+    .catch((e: ResponseError) => {
+      setErr(e.message);
+    })
+  }
+
   return (
     <Modal
       show={props.show}
@@ -25,10 +59,27 @@ export function CouponFormDialog(
       <Modal.Body>
         <FullscreenSingleCol>
           <>
+            <div className="text-center">
+              <h6>Attach a coupon to this price</h6>
+              <PriceHighlight
+                parts={
+                  stripePriceFormat(props.price)
+                    .formatToParts()
+                }
+              />
+            </div>
 
+            <ErrorAlert
+              msg={err}
+              onClose={() => setErr('')}
+            />
+            <CouponForm
+              onSubmit={handleSubmit}
+            />
           </>
         </FullscreenSingleCol>
       </Modal.Body>
     </Modal>
   );
 }
+
