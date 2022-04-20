e674d077dc86c4e3cc1eb4504d995fdfbda8ad16
weiguo.ni <neefrankie@gmail.com>
2022-04-20 16:58:11 +0800

    refactor

--- src/features/stripe/CouponItem.tsx
+++ src/features/stripe/CouponItem.tsx
@@ -1,8 +1,8 @@
+import { formatISO } from 'date-fns';
 import { Card } from 'react-bootstrap';
 import { TableBody, TRow } from '../../components/list/Table';
 import { ModeBadge } from '../../components/text/Badge';
-import { formatMoney } from '../../data/money-parts';
-import { StripeCoupon } from '../../data/stripe-price';
+import { StripeCoupon, foramtCouponAmount } from '../../data/stripe-price';
 
 export function CouponItem(
   props: {
@@ -19,12 +19,24 @@ export function CouponItem(
       data: [props.coupon.status],
     },
     {
-      head: 'Promotion Period',
-      data: [`${props.coupon.startUtc} - ${props.coupon.endUtc}`]
+      head: 'Promotion Start',
+      data: [
+        props.coupon.startUtc || 'NULL'
+      ]
+    },
+    {
+      head: 'Promotion End',
+      data: [
+        props.coupon.endUtc || 'NULL'
+      ]
     },
     {
       head: 'Redeem before',
-      data: [(new Date(props.coupon.redeemBy)).toISOString()]
+      data: [
+        props.coupon.redeemBy > 0
+          ? formatISO(new Date(props.coupon.redeemBy))
+          : 'NULL'
+      ]
     },
     {
       head: 'Live',
@@ -36,10 +48,15 @@ export function CouponItem(
 
   return (
     <Card>
-      <Card.Header>{props.coupon.name}</Card.Header>
+      <Card.Header>
+        {props.coupon.name}
+      </Card.Header>
       <Card.Body>
         <Card.Title className="text-center">
-          - {formatMoney(props.coupon.currency, props.coupon.amountOff)}
+          -
+          {
+            foramtCouponAmount(props.coupon)
+          }
         </Card.Title>
 
         <table className="table table-borderless">
