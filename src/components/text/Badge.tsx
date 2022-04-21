eab617698e7dc98677cf236f24186004617aaf37
weiguo.ni <neefrankie@gmail.com>
2022-04-21 17:38:43 +0800

    refactor: TimeZoneBadge

--- src/components/text/Badge.tsx
+++ src/components/text/Badge.tsx
@@ -24,21 +24,11 @@ export function ActiveBadge(
   return <Badge bg="secondary">Inactive</Badge>;
 }
 
-export function TimezoneBadge(
-  props: {
-    offset?: string
-  }
-) {
-  const offset = props.offset || isoOffset(new Date());
-
+export function TimezoneGuide() {
 
   return (
     <div className="mb-3 text-muted">
-      <div>
-        当前时区
-        <Badge bg="info">{offset}</Badge>
-      </div>
-      <div>时间按你当前所处时区选择即可，不需要做转换。数据提交后会统一转换为0区ISO8601标准时间格式</div>
+      时间按你当前所处时区选择即可，不需要做转换。数据提交后会统一转换为0区ISO8601标准时间格式
     </div>
   );
 }
