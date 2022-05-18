8651080bb68694c67a96a0022547cff196b3108d
weiguo.ni <neefrankie@gmail.com>
2022-05-18 22:27:12 +0800

    refactor: data paging

--- src/data/paged-list.ts
+++ src/data/paged-list.ts
@@ -1,19 +1,31 @@
-export interface PagedList<T> {
+export type PageCount = {
   total: number;
   page: number;
   limit: number;
+}
+export type PagedList<T> = PageCount & {
   data: T[];
 }
 
-export interface PagingParams {
-  currentPage: number;
+export type PagedNavParams = {
+  prevNext: number;
   itemsPerPage: number;
 }
 
-export function pagingToQuery(p: PagingParams): URLSearchParams {
-  const params = new URLSearchParams();
-  params.set('page', p.currentPage.toFixed());
-  params.set('per_page', p.itemsPerPage.toFixed())
+export function serializePagingQuery(params: PagedNavParams): URLSearchParams {
+  const search = new URLSearchParams();
+  search.set('page', params.prevNext.toFixed())
+  search.set('per_page', params.itemsPerPage.toFixed());
 
-  return params;
+  return search;
+}
+
+export function parsePagingQuery(query: URLSearchParams): PagedNavParams {
+  const page = query.get('page');
+  const perPage = query.get('per_page');
+
+  return {
+    prevNext: page ? Number.parseInt(page) : 1,
+    itemsPerPage: perPage ? Number.parseInt(perPage) : 20,
+  }
 }
