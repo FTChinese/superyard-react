dff02376a4973ce91d5683175914f3bb728a573d
weiguo.ni <neefrankie@gmail.com>
2022-05-18 22:25:04 +0800

    refactor:

--- src/components/text/SupportMarkdown.tsx
+++ src/components/text/SupportMarkdown.tsx
@@ -1,7 +1,7 @@
 export function SupportMarkdown() {
   return (
     <>
-      Support <a href="https://www.markdownguide.org/basic-syntax/" target="_blank">Markdown</a> syntax.
+      Support <a href="https://www.markdownguide.org/basic-syntax/" target="_blank">Markdown</a> syntax
     </>
   );
 }
