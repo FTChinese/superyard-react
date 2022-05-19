35157c4b9ba58a150720337f818bfb62393d21d7
weiguo.ni <neefrankie@gmail.com>
2022-05-20 07:12:55 +0800

    refactor: toolbar styles

--- src/components/layout/Toolbar.tsx
+++ src/components/layout/Toolbar.tsx
@@ -1,21 +1,19 @@
 import { Link } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import { Logout } from './Logout';
-import styles from  './Toolbar.module.css';
 
 export function Toolbar() {
   const { passport } = useAuth();
 
   return (
-    <div className={`d-flex justify-content-between ${styles.toolbar}`}>
-      <div className={styles.ftcBrand}>
-        <Link to="/">FTC Admin CMS</Link>
+    <div className={`container-fluid d-flex justify-content-between align-items-center ftc-toolbar`}>
+      <div className="ftc-brand d-flex align-items-end">
+        <Link to="/">FTC CMS</Link>
       </div>
 
       {passport &&
         <div className="d-flex align-items-center">
           <span className="border-end pe-3">{passport.userName}</span>
-
           <Logout />
         </div>
       }
