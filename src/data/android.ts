3664aa3c25114ffa81a85cdfcf3beb019e00a858
weiguo.ni <neefrankie@gmail.com>
2022-05-20 19:11:45 +0800

    feat: android release

---
+++ src/data/android.ts
@@ -0,0 +1,15 @@
+import { PagedList } from './paged-list';
+
+export type ReleaseParams = {
+  versionName: string;
+  versionCode: number;
+  body?: string;
+  apkUrl: string;
+};
+
+export type Release = ReleaseParams & {
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export type ReleaseList = PagedList<Release>;
