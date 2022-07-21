feaffaab4585f750c5a27ba840d7b1e339be5874
weiguo.ni <neefrankie@gmail.com>
2022-07-21 14:59:18 +0800

    feat: useProgress

---
+++ src/components/hooks/useProgress.tsx
@@ -0,0 +1,24 @@
+import { atom, useRecoilState } from 'recoil';
+
+const progressState = atom<boolean>({
+  key: 'loadingState',
+  default: false,
+});
+
+export function useProgress() {
+  const [progress, setProgress] = useRecoilState(progressState);
+
+  const startProgress = () => {
+    setProgress(true);
+  }
+
+  const stopProgress = () => {
+    setProgress(false);
+  }
+  return {
+    progress,
+    startProgress,
+    stopProgress,
+  }
+}
+
