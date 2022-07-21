c71faba9c0291f9b2d26bda71781ee2365177f0b
weiguo.ni <neefrankie@gmail.com>
2022-07-21 11:32:58 +0800

    refactor: add open property

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +7,8 @@ export default defineConfig({
   server: {
     // port: 5000,
     proxy: {
-      '/api': 'http://localhost:3001'
-    }
-  }
-})
+      '/api': 'http://localhost:3001',
+    },
+    open: true,
+  },
+});
