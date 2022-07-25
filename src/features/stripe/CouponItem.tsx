9ebb47ca0a31d100b558846606dc17fb838349a5
weiguo.ni <neefrankie@gmail.com>
2022-07-25 14:08:09 +0800

    refactor

--- src/features/stripe/CouponItem.tsx
+++ src/features/stripe/CouponItem.tsx
@@ -1,69 +1,137 @@
 import { formatISO } from 'date-fns';
-import { Card } from 'react-bootstrap';
+import { Button, ButtonGroup, Card } from 'react-bootstrap';
+import { Flex } from '../../components/layout/Flex';
 import { TableBody, TRow } from '../../components/list/Table';
-import { ModeBadge } from '../../components/text/Badge';
-import { StripeCoupon, foramtCouponAmount } from '../../data/stripe-price';
+import { CouponStatusBadge, ModeBadge } from '../../components/text/Badge';
+import { StripeCoupon, formatCouponAmount } from '../../data/stripe-price';
+
+export enum CouponAction {
+  Edit,
+  Drop,
+  Activate,
+}
 
 export function CouponItem(
   props: {
     coupon: StripeCoupon;
+    loading: boolean;
+    onAction: (c: StripeCoupon, action: CouponAction) => void;
   }
 ) {
-  const rows: TRow[] = [
+  const rows: TRow[] = buildCouponRow(props.coupon);
+
+  let btn: JSX.Element | null = null;
+  switch (props.coupon.status) {
+    case 'active':
+      btn = (
+        <Button
+          variant='danger'
+          size="sm"
+          disabled={props.loading}
+          onClick={() => props.onAction(props.coupon, CouponAction.Drop)}
+        >
+          Drop
+        </Button>
+      );
+      break;
+
+    case 'paused':
+    case 'cancelled':
+      btn = (
+        <Button
+          variant='outline-primary'
+          size="sm"
+          disabled={props.loading}
+          onClick={() => props.onAction(props.coupon, CouponAction.Activate)}
+        >
+          Activiate
+        </Button>
+      )
+  }
+
+  return (
+    <>
+      <Card>
+        <Card.Header>
+          <Flex
+            start={<span>{props.coupon.name}</span>}
+            end={
+              <ButtonGroup
+                size='sm'
+              >
+                {btn}
+
+                <Button
+                  variant='primary'
+                  size='sm'
+                  onClick={() => props.onAction(props.coupon, CouponAction.Edit)}
+                >
+                  Edit
+                </Button>
+              </ButtonGroup>
+
+            }
+          />
+
+        </Card.Header>
+        <Card.Body>
+          <Card.Title className="text-center">
+            {
+              formatCouponAmount(props.coupon)
+            }
+          </Card.Title>
+
+          <table className="table table-borderless">
+            <TableBody
+              rows={rows}
+            />
+          </table>
+        </Card.Body>
+      </Card>
+    </>
+
+  );
+}
+
+function buildCouponRow(coupon: StripeCoupon): TRow[] {
+  return [
     {
       head: 'ID',
-      data: [props.coupon.id],
+      data: [coupon.id],
     },
     {
       head: 'Status',
-      data: [props.coupon.status],
+      data: [
+        <CouponStatusBadge
+          status={coupon.status}
+        />
+      ],
     },
     {
       head: 'Promotion Start',
       data: [
-        props.coupon.startUtc || 'NULL'
+        coupon.startUtc || 'NULL'
       ]
     },
     {
       head: 'Promotion End',
       data: [
-        props.coupon.endUtc || 'NULL'
+        coupon.endUtc || 'NULL'
       ]
     },
     {
       head: 'Redeem before',
       data: [
-        props.coupon.redeemBy > 0
-          ? formatISO(new Date(props.coupon.redeemBy))
+        coupon.redeemBy > 0
+          ? formatISO(new Date(coupon.redeemBy))
           : 'NULL'
       ]
     },
     {
       head: 'Live',
       data: [
-        <ModeBadge live={props.coupon.liveMode} />
+        <ModeBadge live={coupon.liveMode} />
       ]
     }
   ];
-
-  return (
-    <Card>
-      <Card.Header>
-        {props.coupon.name}
-      </Card.Header>
-      <Card.Body>
-        <Card.Title className="text-center">
-          {
-            foramtCouponAmount(props.coupon)
-          }
-        </Card.Title>
-
-        <table className="table table-borderless">
-          <TableBody
-            rows={rows}
-          />
-        </table>
-      </Card.Body>
-    </Card>
-  );
 }
