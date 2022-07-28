afa4b9679d7fd6f06536a7dd244bb433071e6b06
weiguo.ni <neefrankie@gmail.com>
2022-07-28 20:04:10 +0800

    feat: formatMoney

--- src/data/localization.ts
+++ src/data/localization.ts
@@ -1,6 +1,6 @@
 import { Cycle, PaymentKind, Tier } from './enum';
 import { Edition } from './edition';
-import { ymdToCycle, YearMonthDay } from './ymd';
+import { cycleOfYMD, YearMonthDay } from './ymd';
 
 const tiers: Record<Tier, string> = {
   standard: '标准会员',
@@ -40,17 +40,56 @@ export function localizedEdition(e: Edition): string {
   return `${localizedTier(e.tier)}/${localizedCycle(e.cycle)}`;
 }
 
-export function formatEdition(t: Tier, ymd: YearMonthDay): string {
-  return localizedEdition({
-    tier: t,
-    cycle: ymdToCycle(ymd)
-  });
+export function formatMoney(currency: string, amount: number): string {
+  return new Intl.NumberFormat(undefined, {
+      style: 'currency',
+      currency: currency.toUpperCase(),
+      currencyDisplay: 'narrowSymbol',
+    })
+    .format(amount);
 }
 
-export function localizeBool(b: boolean): string {
-  if (b) {
-    return "True"
-  }
+export type MoneyParts = {
+  symbol: string;
+  integer: string;
+  decimal: string;
+};
 
-  return 'False'
+export type PriceParts = MoneyParts & {
+  cycle: string;
+}
+
+export function concatPriceParts(parts: PriceParts): string {
+  return `${parts.symbol}${parts.integer}${parts.decimal}${parts.cycle}`;
+}
+
+export function newMoneyParts(currency: string, amount: number): MoneyParts {
+  return new Intl.NumberFormat(undefined, {
+      style: 'currency',
+      currency: currency.toUpperCase(),
+      currencyDisplay: 'narrowSymbol',
+    })
+    .formatToParts(amount)
+    .reduce<MoneyParts>((prev, curr) => {
+      switch (curr.type) {
+        case 'currency':
+          prev.symbol = curr.value;
+          break;
+
+        case 'integer':
+          prev.integer += curr.value;
+          break;
+
+        case 'decimal':
+        case 'fraction':
+          prev.decimal += curr.value;
+          break;
+      }
+
+      return prev;
+    }, {
+      symbol: '',
+      integer: '',
+      decimal: '',
+    })
 }
