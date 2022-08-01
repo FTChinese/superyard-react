784052208935e1c3fd0a75f55075226b810434dd
weiguo.ni <neefrankie@gmail.com>
2022-08-01 11:13:09 +0800

    change prop.reader to not null

--- src/features/readers/ReaderDetailScreen.tsx
+++ src/features/readers/ReaderDetailScreen.tsx
@@ -1,5 +1,4 @@
 import { CMSPassport } from '../../data/cms-account';
-import { Membership, zeroMember } from '../../data/membership';
 import { ReaderAccount } from '../../data/reader-account';
 import { AccountDetails } from './AccountDetails';
 import { MemberDetails } from './MemberDetails';
@@ -8,32 +7,33 @@ import { MemberMenu } from './MemberMenu';
 export function ReaderDetailScreen(
   props: {
     passport: CMSPassport;
-    reader?: ReaderAccount;
-    onMemberModified: (m: Membership) => void;
+    reader: ReaderAccount;
+    onDropMember: () => void;
+    onClickUpsert: () => void;
   }) {
   return (
     <>
-      {props.reader &&
-        <section>
-          <AccountDetails
-            reader={props.reader} />
-        </section>}
+      <section>
+        <AccountDetails
+          reader={props.reader}
+        />
+      </section>
 
-      {props.reader &&
-        <section>
-          <MemberDetails
-            jwtToken={props.passport.token}
-            reader={props.reader}
-            onDropped={() => props.onMemberModified(zeroMember())} />
-        </section>}
+      <section>
+        <MemberDetails
+          jwtToken={props.passport.token}
+          reader={props.reader}
+          onClickDrop={props.onDropMember}
+        />
+      </section>
 
-      {props.reader &&
-        <section>
-          <MemberMenu
-            jwtToken={props.passport.token}
-            reader={props.reader}
-            onUpserted={props.onMemberModified} />
-        </section>}
+      <section>
+        <MemberMenu
+          jwtToken={props.passport.token}
+          reader={props.reader}
+          onClickUpsert={props.onClickUpsert}
+        />
+      </section>
     </>
   );
 }
