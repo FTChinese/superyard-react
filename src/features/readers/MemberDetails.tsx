0023aee6d7955dede7d308cb788b7b04a7574b6d
weiguo.ni <neefrankie@gmail.com>
2022-08-01 11:12:24 +0800

    removed DropMemberDialog

--- src/features/readers/MemberDetails.tsx
+++ src/features/readers/MemberDetails.tsx
@@ -3,57 +3,41 @@ import Card from 'react-bootstrap/Card';
 import { Table, TableBody, TRow } from '../../components/list/Table';
 import { isZeroMember, Membership } from '../../data/membership';
 import { SmallButton } from '../../components/buttons/Button';
-import { getCompoundId, ReaderAccount } from '../../data/reader-account';
-import { useEffect, useState } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import { LoadButton } from '../../components/buttons/Button';
-import { useMemberState } from './useMemberState';
-import { toast } from 'react-toastify';
+import { ReaderAccount } from '../../data/reader-account';
 
 export function MemberDetails(
   props: {
     jwtToken: string;
     reader: ReaderAccount;
-    onDropped: () => void;
+    onClickDrop: () => void;
   }
 ) {
   const rows = buildMemberRows(props.reader.membership);
-  const [showDrop, setShowDrop] = useState(false);
 
   return (
-    <>
-      <Card>
-        <Card.Header>
-          <Stack direction='horizontal'>
-            <span>Membership Details</span>
-            {
-              !isZeroMember(props.reader.membership) &&
-              <SmallButton
-                onClick={() => setShowDrop(true)}
-                className="ms-auto"
-                variant="danger"
-              >
-                Drop
-              </SmallButton>
-            }
-          </Stack>
-        </Card.Header>
+    <Card>
+      <Card.Header>
+        <Stack direction='horizontal'>
+          <span>Membership Details</span>
+          {
+            !isZeroMember(props.reader.membership) &&
+            <SmallButton
+              onClick={props.onClickDrop}
+              className="ms-auto"
+              variant="danger"
+            >
+              Drop
+            </SmallButton>
+          }
+        </Stack>
+      </Card.Header>
 
-        <Table>
-          <TableBody
-            rows={rows}
-          />
-        </Table>
-      </Card>
-
-      <DropMemberDialog
-        token={props.jwtToken}
-        compoundId={getCompoundId(props.reader)}
-        show={showDrop}
-        onHide={() => setShowDrop(false)}
-        onDropped={props.onDropped}
-      />
-    </>
+      <Table>
+        <TableBody
+          rows={rows}
+        />
+      </Table>
+    </Card>
   );
 }
 
@@ -114,52 +98,4 @@ function buildMemberRows(m: Membership): TRow[] {
   ];
 }
 
-function DropMemberDialog(
-  props: {
-    token: string;
-    compoundId: string;
-    show: boolean;
-    onHide: () => void;
-    onDropped: () => void;
-  }
-) {
-  const {
-    progress,
-    errMsg,
-    dropMember,
-  } = useMemberState();
 
-  useEffect(() => {
-    if (errMsg) {
-      toast.error(errMsg);
-    }
-  }, [errMsg])
-  return (
-    <Modal
-      show={props.show}
-      onHide={props.onHide}
-    >
-      <Modal.Header closeButton>
-        Delete Membership
-      </Modal.Header>
-      <Modal.Body>
-        <p>Are you sure you want to delete this user's membership?</p>
-      </Modal.Body>
-
-      <Modal.Footer>
-        <LoadButton
-          text='Delete'
-          progress={progress}
-          onClick={() => {
-            dropMember(props.token, props.compoundId)
-              .then(ok => {
-                if (ok) {
-                  props.onDropped();
-                }
-              })
-          }}
-        />
-      </Modal.Footer>
-    </Modal>
-  );
-}
