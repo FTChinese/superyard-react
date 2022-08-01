5d088844367fe98f175f60213534d2b2b7fc236d
weiguo.ni <neefrankie@gmail.com>
2022-08-01 16:45:29 +0800

    WxDetailPage

---
+++ src/pages/readers/WxDetailPage.tsx
@@ -0,0 +1,26 @@
+import { useParams } from 'react-router-dom';
+import { useAuth } from '../../components/hooks/useAuth';
+import { Missing, Unauthorized } from '../../components/routes/Unauthorized';
+import { AccountKind } from '../../data/enum';
+import { ReaderDetailPageScreen } from './ReaderDetailPageScreen';
+
+export function WxDetailPage() {
+   const { id } = useParams<'id'>();
+  const { passport } = useAuth();
+
+  if (!id) {
+    return <Missing message="Missing user id" />;
+  }
+
+  if (!passport) {
+    return <Unauthorized />;
+  }
+
+  return (
+    <ReaderDetailPageScreen
+      id={id}
+      kind={AccountKind.Wechat}
+      passport={passport}
+    />
+  );
+}
