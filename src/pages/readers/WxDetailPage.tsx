7b19eff88894a66dfe4b1e255d5904c98b18c0a2
weiguo.ni <neefrankie@gmail.com>
2022-08-12 16:53:02 +0800

    refactor import path

--- src/pages/readers/WxDetailPage.tsx
+++ src/pages/readers/WxDetailPage.tsx
@@ -1,11 +1,11 @@
 import { useParams } from 'react-router-dom';
 import { useAuth } from '../../components/hooks/useAuth';
-import { Missing, Unauthorized } from '../../components/routes/Unauthorized';
+import { Missing, Unauthorized } from '../../components/middleware/Unauthorized';
 import { AccountKind } from '../../data/enum';
 import { ReaderDetailPageScreen } from './ReaderDetailPageScreen';
 
 export function WxDetailPage() {
-   const { id } = useParams<'id'>();
+  const { id } = useParams<'id'>();
   const { passport } = useAuth();
 
   if (!id) {
