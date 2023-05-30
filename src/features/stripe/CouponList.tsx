302e8ff9d469f77b10306650a7bbf086e7874b09
weiguo.ni <neefrankie@gmail.com>
2023-05-30 17:35:35 +0800

    simplify coupon list

--- src/features/stripe/CouponList.tsx
+++ src/features/stripe/CouponList.tsx
@@ -1,14 +1,10 @@
 import Button from 'react-bootstrap/Button';
-import { StripeCoupon } from '../../data/stripe-price';
-import { CouponAction, CouponItem } from './CouponItem';
 import Stack from 'react-bootstrap/Stack';
 
-export function CouponList(
+export function CouponListSection(
   props: {
-    coupons: StripeCoupon[];
-    handlingCoupon: boolean;
     onNewCoupon: () => void;
-    onModifyCoupon: (c: StripeCoupon, action: CouponAction) => void;
+    children: JSX.Element | null;
   }
 ) {
   return (
@@ -23,16 +19,7 @@ export function CouponList(
           New
         </Button>
       </Stack>
-      {
-        props.coupons.map((c) => (
-          <CouponItem
-            key={c.id}
-            coupon={c}
-            loading={props.handlingCoupon}
-            onAction={props.onModifyCoupon}
-          />
-        ))
-      }
+      {props.children}
     </section>
   );
 }
