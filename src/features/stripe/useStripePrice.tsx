6305903296f094ba17dc4f9297dd577bad09d98c
weiguo.ni <neefrankie@gmail.com>
2023-05-30 14:34:00 +0800

    refactor: stripe price refresh, activation

--- src/features/stripe/useStripePrice.tsx
+++ src/features/stripe/useStripePrice.tsx
@@ -5,28 +5,87 @@ import { isOneTime } from '../../data/enum';
 import { StripeCoupon, StripePrice } from '../../data/stripe-price';
 import { ReqConfig } from '../../http/ReqConfig';
 import { ResponseError } from '../../http/response-error';
-import { activateStripeCoupon, loadStripeCoupons, loadStripePrice } from '../../repository/stripe';
+import { activateStripeCoupon, activateStripePrice, loadStripeCoupons, loadStripePrice, updateStripePriceMeta } from '../../repository/stripe';
+import { FormikHelpers } from 'formik';
+import { StripePriceFormVal, buildStripePriceParams } from './StripePriceForm';
 
 export function useStripePrice() {
   const { startProgress, stopProgress } = useProgress();
 
   const [price, setPrice] = useState<StripePrice>();
+  const [showPriceActivate, setShowPriceActivate] = useState(false);
+
   const [coupons, setCoupons] = useState<StripeCoupon[]>([]);
 
+  // Load a stripe price, or force refreshing by passing `refresh: true`.
   const loadPrice = (priceId: string, config: ReqConfig) => {
     startProgress();
-    setPrice(undefined);
+    if (!config.refresh) {
+      setPrice(undefined);
+    }
 
     loadStripePrice(priceId, config)
       .then((sp) => {
 
         setPrice(sp);
+        stopProgress();
+
         if (isOneTime(sp.kind)) {
-          stopProgress();
           return;
         }
 
-        listCoupns(priceId, config);
+        // Continue to load coupons.
+        if (!config.refresh) {
+          listCoupns(priceId, config);
+        } else {
+          toast.info('Refresh succeeded!')
+        }
+      })
+      .catch((err: ResponseError) => {
+        stopProgress();
+        toast.error(err.message);
+      });
+  }
+
+  // updatePriceMeta returns s closure to meet
+  // Formik's submit signature.
+  const updatePriceMeta = (p: StripePrice, config: ReqConfig) => {
+    return (
+      values: StripePriceFormVal,
+      helpers: FormikHelpers<StripePriceFormVal>
+    ) => {
+      console.log(values);
+
+      const body = buildStripePriceParams(values, p);
+
+      helpers.setSubmitting(true);
+
+      updateStripePriceMeta(p.id, body, config)
+        .then((newPrice) => {
+          helpers.setSubmitting(false);
+          toast.success('Saved!');
+          setPrice(newPrice);
+        })
+        .catch((err: ResponseError) => {
+          helpers.setSubmitting(false);
+          if (err.statusCode === 422) {
+            helpers.setErrors(err.toFormFields);
+            return;
+          }
+          toast.error(err.message);
+        });
+    };
+  };
+
+  const activatePrice = (p: StripePrice, cofig: ReqConfig) => {
+    startProgress();
+
+    activateStripePrice(p, cofig)
+      .then((newPrice) => {
+        stopProgress();
+        setPrice(newPrice);
+        toast.info(`${p.onPaywall ? 'Deactivate' : 'Activate'} price succeeded!`);
+        setShowPriceActivate(false);
       })
       .catch((err: ResponseError) => {
         stopProgress();
@@ -35,6 +94,7 @@ export function useStripePrice() {
   }
 
   const listCoupns = (priceId: string, config: ReqConfig) => {
+    startProgress()
     loadStripeCoupons(priceId, config)
       .then((c) => {
         stopProgress();
@@ -80,6 +140,13 @@ export function useStripePrice() {
   return {
     price,
     loadPrice,
+
+    activatePrice,
+    showPriceActivate,
+    setShowPriceActivate,
+
+    updatePriceMeta,
+
     coupons,
     onCouponCreated,
     onCouponUpdated,
