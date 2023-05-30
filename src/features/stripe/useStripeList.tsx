385e2f96ed399952dc288b3613d771a5f71a76a2
weiguo.ni <neefrankie@gmail.com>
2023-05-30 15:15:55 +0800

    removed update stripe price meta

--- src/features/stripe/useStripeList.tsx
+++ src/features/stripe/useStripeList.tsx
@@ -1,31 +1,31 @@
 import { useState } from 'react';
 import { StripePrice, StripePriceList } from '../../data/stripe-price';
 import { ReqConfig } from '../../http/ReqConfig';
-import { listStripePrices, loadStripePrice, updateStripePriceMeta } from '../../repository/stripe';
+import { listStripePrices, loadStripePrice } from '../../repository/stripe';
 import { ResponseError } from '../../http/response-error';
 import { toast } from 'react-toastify';
-import { StripePriceFormVal, buildStripePriceParams } from './StripePriceForm';
-import { FormikHelpers } from 'formik';
 import { PagingQuery } from '../../http/paged-list';
+import { useProgress } from '../../components/hooks/useProgress';
 
 export function useStripeList() {
-  const [loadingList, setLoadingList] = useState(false);
+
+  const { startProgress, stopProgress } = useProgress();
   const [pagedPrices, setPagedPrices] = useState<StripePriceList>();
 
   const [loadingPrice, setLoadingPrice] = useState(false);
   const [price, setPrice] = useState<StripePrice>();
 
   const listPrices = (config: ReqConfig, page: PagingQuery) => {
-    setLoadingList(true);
+    startProgress();
 
     listStripePrices(config, page)
       .then((list) => {
-        setLoadingList(false);
+        stopProgress();
         setPagedPrices(list);
       })
       .catch((err: ResponseError) => {
         console.log(err);
-        setLoadingList(false);
+        stopProgress()
         toast.error(err.message);
       });
   }
@@ -47,45 +47,13 @@ export function useStripeList() {
       });
   }
 
-  // updatePriceMeta returns s closure to meet
-  // Formik's submit signature.
-  const updatePriceMeta = (p: StripePrice, config: ReqConfig) => {
-    return (
-      values: StripePriceFormVal,
-      helpers: FormikHelpers<StripePriceFormVal>
-    ) => {
-      console.log(values);
-
-      const body = buildStripePriceParams(values, p);
-
-      helpers.setSubmitting(true);
-
-      updateStripePriceMeta(p.id, body, config)
-        .then((newPrice) => {
-          helpers.setSubmitting(false);
-          toast.success('Saved!');
-          setPrice(newPrice);
-        })
-        .catch((err: ResponseError) => {
-          helpers.setSubmitting(false);
-          if (err.statusCode === 422) {
-            helpers.setErrors(err.toFormFields);
-            return;
-          }
-          toast.error(err.message);
-        });
-    };
-  };
-
   return {
-    loadingList,
     pagedPrices,
     listPrices,
 
     loadingPrice,
     loadPrice,
     price,
-    updatePriceMeta,
   }
 }
 
