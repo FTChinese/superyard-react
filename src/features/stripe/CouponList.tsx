4e2c2dda0cdba3bc8fd4cca6d44cd74cad3ec569
weiguo.ni <neefrankie@gmail.com>
2023-05-31 09:56:50 +0800

    add Pull button

--- src/features/stripe/CouponList.tsx
+++ src/features/stripe/CouponList.tsx
@@ -1,24 +1,36 @@
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Button from 'react-bootstrap/Button';
 import Stack from 'react-bootstrap/Stack';
 
 export function CouponListSection(
   props: {
     onNewCoupon: () => void;
-    children: JSX.Element | null;
+    children: JSX.Element | null; // Render a list of coupons.
   }
 ) {
   return (
     <section className="mb-3">
       <Stack direction="horizontal">
         <h4>Price Coupons</h4>
-        <Button
+        <ButtonGroup
           className="ms-auto"
-          variant="primary"
-          onClick={props.onNewCoupon}
         >
-          New
-        </Button>
+          <Button
+            variant="outline-primary"
+          >
+            Pull
+          </Button>
+          <Button
+            onClick={props.onNewCoupon}
+          >
+            New
+          </Button>
+        </ButtonGroup>
       </Stack>
+      <ul>
+        <li>Click New button to create a new coupon without visiting Stripe dashboard</li>
+        <li>Click Pull button to sync an existing coupon to FTC's database.</li>
+      </ul>
       {props.children}
     </section>
   );
