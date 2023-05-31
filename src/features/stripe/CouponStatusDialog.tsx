c910e203872d0edc31f38d3e6ea3f392e32c09c6
weiguo.ni <neefrankie@gmail.com>
2023-05-31 17:05:02 +0800

    refactor

--- src/features/stripe/CouponStatusDialog.tsx
+++ src/features/stripe/CouponStatusDialog.tsx
@@ -2,44 +2,60 @@ import Modal from 'react-bootstrap/Modal';
 import { ModeBadge } from '../../components/text/Badge';
 import { formatCouponAmount, StripeCoupon } from '../../data/stripe-price';
 import { isActiveDiscount } from '../../data/enum';
-import Button from 'react-bootstrap/Button';
+import { useCoupon } from './useCoupon';
+import { CMSPassport } from '../../data/cms-account';
+import { LoadButton } from '../../components/buttons/Button';
 
 /**
  * Activate or drop a stripe coupon.
  */
 export function CouponStatusDialog(
   props: {
+    passport: CMSPassport
     live: boolean;
     coupon: StripeCoupon;
     show: boolean;
     onHide: () => void;
-    onConfirm: (c: StripeCoupon) => void;
-    progress: boolean;
+    onSaved: (c: StripeCoupon) => void;
   }
 ) {
 
   const isActive = isActiveDiscount(props.coupon.status);
 
+  const {
+    clear,
+    changeStatus,
+    changing,
+  } = useCoupon();
+
   return (
     <Modal
       show={props.show}
-      onHide={props.onHide}
+      onHide={() => {
+        clear();
+        props.onHide();
+      }}
     >
       <Modal.Header closeButton>
-        <Modal.Title className="me-3">{ isActive ? 'Drop' : 'Activate' } Coupon {formatCouponAmount(props.coupon)}</Modal.Title>
+        <Modal.Title className="me-3">{ isActive ? 'Cancel' : 'Activate' } Coupon {formatCouponAmount(props.coupon)}</Modal.Title>
         <ModeBadge live={props.live} />
       </Modal.Header>
 
       <Modal.Body>
-        Are you sure you want to drop this coupon ?
+        Are you sure you want to { isActive ? 'cancel' : 'activate' } this coupon ?
       </Modal.Body>
       <Modal.Footer>
-        <Button
-          onClick={() => props.onConfirm(props.coupon)}
-          disabled={props.progress}
-        >
-          Yes
-        </Button>
+        <LoadButton
+          text='Yes'
+          disabled={changing}
+          progress={changing}
+          onClick={() => {
+            changeStatus(props.coupon, {
+              live: props.live,
+              token: props.passport.token,
+            }, props.onSaved);
+          }}
+        />
       </Modal.Footer>
     </Modal>
   );
