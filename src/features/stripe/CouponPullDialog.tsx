5041554f02a9f63029e2513dde63d05cd274125f
weiguo.ni <neefrankie@gmail.com>
2023-05-31 11:15:20 +0800

    refactor: pull stripe coupon

--- src/features/stripe/CouponPullDialog.tsx
+++ src/features/stripe/CouponPullDialog.tsx
@@ -1,13 +1,10 @@
 import { FormikHelpers } from 'formik';
 import { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
-import { FullscreenSingleCol } from '../../components/layout/FullscreenSingleCol';
 import { ModeBadge } from '../../components/text/Badge';
-import { ErrorAlert } from '../../components/text/ErrorAlert';
 import { PriceHighlight } from '../../components/text/PriceHighlight';
 import { CMSPassport } from '../../data/cms-account';
 import {
-  formatCouponAmount,
   newStripePriceParts,
   StripeCoupon,
   StripePrice,
@@ -15,8 +12,13 @@ import {
 import { ResponseError } from '../../http/response-error';
 import { upsertStripeCoupon } from '../../repository/stripe';
 import { buildCouponParams, CouponForm, CouponFormVal } from './CouponForm';
+import { FullscreenTwoCols } from '../../components/layout/FullscreenTwoCols';
+import { useCouon } from './useCoupon';
+import { SearchBox } from '../../components/forms/SearchBox';
+import { toast } from 'react-toastify';
+import { CouponCard } from './CouponCard';
 
-export function CouponUpsertDialog(props: {
+export function CouponPullDialog(props: {
   passport: CMSPassport;
   live: boolean;
   price: StripePrice;
@@ -51,6 +53,13 @@ export function CouponUpsertDialog(props: {
       });
   };
 
+  // ---------
+  const {
+    searching,
+    getCoupon,
+    coupon,
+  } = useCouon();
+
   return (
     <Modal
       show={props.show}
@@ -59,45 +68,60 @@ export function CouponUpsertDialog(props: {
     >
       <Modal.Header closeButton>
         <Modal.Title className="me-3">
-          {
-            props.coupon ? 'Update Coupon' : 'Create Coupon'
-          }
+          Pull a coupon from Stripe and edit it
         </Modal.Title>
         <ModeBadge live={props.live} />
       </Modal.Header>
 
       <Modal.Body>
-        <FullscreenSingleCol>
-          <>
-            <div className="text-center">
-              <h6>
-                {
-                  props.coupon ?
-                    'Modify an existing coupon under price' :
-                    'Attach a coupon to this price'
-                }
-              </h6>
-              <PriceHighlight
-                parts={
-                  newStripePriceParts(props.price)
-                }
-              />
-              {
-                props.coupon &&
-                <div>{formatCouponAmount(props.coupon)}</div>
-              }
-            </div>
+        <div className='text-center mb-5'>
+          <h6>
+            Coupon attached to this price
+          </h6>
+          <PriceHighlight
+            parts={
+              newStripePriceParts(props.price)
+            }
+          />
+        </div>
 
-            <ErrorAlert
-              msg={err}
-              onClose={() => setErr('')}
-            />
-            <CouponForm
+        <FullscreenTwoCols
+          right={
+            coupon && <CouponForm
               onSubmit={handleSubmit}
-              coupon={props.coupon}
+              coupon={coupon}
             />
+          }
+        >
+          <>
+            <SearchBox
+              controlId='s'
+              onSubmit={(id) => {
+                getCoupon(id, {
+                  live: props.live,
+                  token: props.passport.token
+                })
+                  .then(result => {
+                    if (result.error) {
+                      toast.error(result.error);
+                    }
+                  });
+              }}
+              label='Search Stripe Coupon'
+              progress={searching}
+              disabled={searching}
+              placeholder='Enter Stripe Coupon ID'
+              desc={"Copy coupon id from Stripe dashboard and it will be synced to FTC's database"}
+            />
+
+            {
+              coupon && <CouponCard
+                coupon={coupon}
+                menu={null}
+              />
+            }
           </>
-        </FullscreenSingleCol>
+        </FullscreenTwoCols>
       </Modal.Body>
     </Modal>
   );
