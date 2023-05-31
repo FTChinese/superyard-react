6e491e10df567f98f99b96093bf592378b4e6a86
weiguo.ni <neefrankie@gmail.com>
2023-05-31 17:04:38 +0800

    refactor: coupon pull dialog

--- src/features/stripe/CouponPullDialog.tsx
+++ src/features/stripe/CouponPullDialog.tsx
@@ -1,5 +1,3 @@
-import { FormikHelpers } from 'formik';
-import { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import { ModeBadge } from '../../components/text/Badge';
 import { PriceHighlight } from '../../components/text/PriceHighlight';
@@ -9,62 +7,37 @@ import {
   StripeCoupon,
   StripePrice,
 } from '../../data/stripe-price';
-import { ResponseError } from '../../http/response-error';
-import { upsertStripeCoupon } from '../../repository/stripe';
-import { buildCouponParams, CouponForm, CouponFormVal } from './CouponForm';
+import { CouponForm } from './CouponForm';
 import { FullscreenTwoCols } from '../../components/layout/FullscreenTwoCols';
-import { useCouon } from './useCoupon';
+import { useCoupon } from './useCoupon';
 import { SearchBox } from '../../components/forms/SearchBox';
-import { toast } from 'react-toastify';
 import { CouponCard } from './CouponCard';
 
 export function CouponPullDialog(props: {
   passport: CMSPassport;
   live: boolean;
   price: StripePrice;
-  coupon?: StripeCoupon; // Present upon editing.
   show: boolean;
   onHide: () => void;
-  onCreated: (c: StripeCoupon) => void;
+  onSaved: (c: StripeCoupon) => void;
 }) {
-  const [err, setErr] = useState('');
 
-  const handleSubmit = (
-    values: CouponFormVal,
-    helpers: FormikHelpers<CouponFormVal>
-  ) => {
-    helpers.setSubmitting(true);
-
-    upsertStripeCoupon(
-        values.couponId,
-        buildCouponParams(props.price.id, values),
-        {
-          live: props.live,
-          token: props.passport.token,
-        }
-      )
-      .then((c) => {
-        props.onCreated(c);
-        helpers.setSubmitting(false);
-      })
-      .catch((e: ResponseError) => {
-        setErr(e.message);
-        helpers.setSubmitting(false);
-      });
-  };
-
-  // ---------
   const {
-    searching,
+    loading,
     getCoupon,
-    coupon,
-  } = useCouon();
+    couponFound,
+    clear,
+    attachCoupon,
+  } = useCoupon();
 
   return (
     <Modal
       show={props.show}
       fullscreen={true}
-      onHide={props.onHide}
+      onHide={() => {
+        clear();
+        props.onHide();
+      }}
     >
       <Modal.Header closeButton>
         <Modal.Title className="me-3">
@@ -87,9 +60,19 @@ export function CouponPullDialog(props: {
 
         <FullscreenTwoCols
           right={
-            coupon && <CouponForm
-              onSubmit={handleSubmit}
-              coupon={coupon}
+            couponFound &&
+            <CouponForm
+              onSubmit={
+                attachCoupon(
+                  props.price,
+                  {
+                    live: props.live,
+                    token: props.passport.token,
+                  },
+                  props.onSaved,
+                )
+              }
+              coupon={couponFound}
             />
           }
         >
@@ -100,23 +83,18 @@ export function CouponPullDialog(props: {
                 getCoupon(id, {
                   live: props.live,
                   token: props.passport.token
-                })
-                  .then(result => {
-                    if (result.error) {
-                      toast.error(result.error);
-                    }
-                  });
+                });
               }}
               label='Search Stripe Coupon'
-              progress={searching}
-              disabled={searching}
+              progress={loading}
+              disabled={loading}
               placeholder='Enter Stripe Coupon ID'
               desc={"Copy coupon id from Stripe dashboard and it will be synced to FTC's database"}
             />
 
             {
-              coupon && <CouponCard
-                coupon={coupon}
+              couponFound && <CouponCard
+                coupon={couponFound}
                 menu={null}
               />
             }
