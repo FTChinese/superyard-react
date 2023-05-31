9b4d5f20ff337c99849cf56a4de69d387c52659a
weiguo.ni <neefrankie@gmail.com>
2023-05-31 17:05:18 +0800

    useCoupon hook

--- src/features/stripe/useCoupon.tsx
+++ src/features/stripe/useCoupon.tsx
@@ -1,41 +1,191 @@
 import { useState } from 'react';
 import { ReqConfig } from '../../http/ReqConfig';
-import { loadStripeCoupon } from '../../repository/stripe';
-import { StripeCoupon } from '../../data/stripe-price';
+import { activateStripeCoupon, deleteStripeCoupon, loadStripeCoupon, updateStripeCoupon } from '../../repository/stripe';
+import { StripeCoupon, StripePrice } from '../../data/stripe-price';
 import { ResponseError } from '../../http/response-error';
-import { Result } from '../../http/result';
+import { CouponFormVal, buildCouponParams } from './CouponForm';
+import { FormikHelpers } from 'formik';
+import { isActiveDiscount } from '../../data/enum';
+import { useProgress } from '../../components/hooks/useProgress';
+import { toast } from 'react-toastify';
 
-export function useCouon() {
+export function useCoupon() {
   // When loading a coupon
-  const [searching, setSearching] = useState(false);
-  const [coupon, setCoupon] = useState<StripeCoupon>();
+  const [loading, setLoading] = useState(false);
+  const [couponFound, setCouponFound] = useState<StripeCoupon>();
 
-  const getCoupon = (id: string, config: ReqConfig): Promise<Result> => {
-    setSearching(true);
+  // Activating/Cancelling coupon
+  const [changing, setChanging] = useState(false);
+  // Refresh progress.
+  const { startProgress, stopProgress } = useProgress();
 
-    return loadStripeCoupon(id, config)
+  const getCoupon = (id: string, config: ReqConfig) => {
+    setLoading(true);
+
+    loadStripeCoupon(id, config)
       .then(c => {
-        setSearching(false);
-        setCoupon(c);
-        return Result.ok();
+        setLoading(false);
+        setCouponFound(c);
       })
       .catch((err: ResponseError) => {
-        setSearching(false);
-        return Result.error(err.message);
+        setLoading(false);
+        toast.error(err.message);
       });
   };
 
+  const refreshCoupon = (id: string, config: ReqConfig): Promise<StripeCoupon | undefined> => {
+    config.refresh = true;
+
+    startProgress();
+
+    return loadStripeCoupon(id, config)
+      .then(c => {
+        stopProgress();
+        toast.info('Refreshed!');
+        return c;
+      })
+      .catch((err: ResponseError) => {
+        stopProgress();
+        toast.error(err.message);
+        return undefined;
+      });
+  }
+
+  /**
+   * @todo
+   */
   const createCoupon = () => {
 
   };
 
-  const updateCoupon = () => {
+  /**
+   * Attach a coupon to current price.
+   */
+  const attachCoupon = (
+    price: StripePrice,
+    config: ReqConfig,
+    onSaved: (c: StripeCoupon) => void,
+  ) => {
+    return (
+      values: CouponFormVal,
+      helpers: FormikHelpers<CouponFormVal>
+    ) => {
+      helpers.setSubmitting(true);
 
+      updateStripeCoupon(
+        values.couponId,
+        buildCouponParams(price.id, values),
+        config,
+      )
+        .then((c) => {
+          helpers.setSubmitting(false);
+          setCouponFound(c);
+          onSaved(c);
+          toast.info('Saved!');
+        })
+        .catch((e: ResponseError) => {
+          helpers.setSubmitting(false);
+          if (e.invalid) {
+            helpers.setErrors(e.toFormFields)
+          } else {
+            toast.info(e.message);
+          }
+        });
+    }
   };
 
+  /**
+   * Change a coupons' effective start and end time.
+   */
+  const updateCoupon = (
+    coupon: StripeCoupon, // Coupon before update.
+    config: ReqConfig,
+    onSaved: (c: StripeCoupon) => void,
+  ) => {
+    return (
+      values: CouponFormVal,
+      helpers: FormikHelpers<CouponFormVal>
+    ) => {
+      if (!coupon.priceId) {
+        toast.error('Coupon to be edited must be already attached to a price!');
+        return;
+      }
+
+      helpers.setSubmitting(true);
+
+      updateStripeCoupon(
+        values.couponId,
+        buildCouponParams(coupon.priceId, values),
+        config,
+      )
+        .then((c) => {
+          helpers.setSubmitting(false);
+          onSaved(c);
+          toast.info('Saved!');
+        })
+        .catch((e: ResponseError) => {
+          helpers.setSubmitting(false);
+          if (e.invalid) {
+            helpers.setErrors(e.toFormFields)
+          } else {
+            toast.error(e.message);
+          }
+        });
+    }
+  };
+
+  // Activate or deactivate a coupon
+  const changeStatus = (
+    coupon: StripeCoupon,
+    config: ReqConfig,
+    onSaved: (c: StripeCoupon) => void,
+  ) => {
+    const isActive = isActiveDiscount(coupon.status);
+
+    setChanging(true);
+
+    if (isActive) {
+      deleteStripeCoupon(coupon.id, config)
+        .then(c => {
+          toast.info('Coupon deactivated!');
+          setChanging(false);
+          onSaved(c)
+        })
+        .catch((err: ResponseError) => {
+          setChanging(false);
+          toast.error(err.message);
+        });
+    } else {
+      activateStripeCoupon(coupon.id, config)
+        .then(c => {
+          toast.info('Coupon activated!');
+          setChanging(false);
+          onSaved(c);
+        })
+        .catch((err: ResponseError) => {
+          toast.error(err.message);
+          setChanging(false);
+        });
+    }
+  };
+
+  const clear = () => {
+    setCouponFound(undefined);
+  }
+
   return {
-    searching,
+    loading,
     getCoupon,
-    coupon,
+    couponFound,
+
+    attachCoupon,
+    updateCoupon,
+
+    changing,
+    changeStatus,
+
+    refreshCoupon,
+
+    clear,
   }
 }
