6c522fd12b6d1b3de05e808da7624a8d16daf0a5
weiguo.ni <neefrankie@gmail.com>
2023-06-01 11:38:02 +0800

    add ReqRefresh

--- src/http/ReqConfig.ts
+++ src/http/ReqConfig.ts
@@ -1,5 +1,9 @@
 export type ReqConfig  = {
   live: boolean; // Live mode or sandbox mode
   token: string; // Json web token set in header.
-  refresh?: boolean
+  refresh?: boolean // Deprecated
+}
+
+export type ReqRefresh = ReqConfig & {
+  refresh: boolean;
 }
