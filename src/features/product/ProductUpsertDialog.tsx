ea20d531029555fece7a3323b4e630127f7fe2c1
weiguo.ni <neefrankie@gmail.com>
2023-06-02 12:51:56 +0800

    refactor

--- src/features/product/ProductUpsertDialog.tsx
+++ src/features/product/ProductUpsertDialog.tsx
@@ -1,19 +1,13 @@
-import { FormikHelpers } from 'formik';
-import { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
-import { toast } from 'react-toastify';
 import { CMSPassport } from '../../data/cms-account';
 import { Product } from '../../data/paywall';
-import { createFtcProduct, updateFtcProduct } from '../../repository/paywall';
-import { ResponseError } from '../../http/response-error';
 import { ModeBadge } from '../../components/text/Badge';
 import { OnProductUpserted } from './callbacks';
 import {
-  ProductFormVal,
   ProductForm,
-  buildNewProductParams,
-  buildUpdateProductParams,
 } from './ProductForm';
+import { useProductUpsert } from './useProductUpsert';
+import { SingleCenterCol } from '../../components/layout/ContentLayout';
 
 /**
  * @description ProductFormDialog is used to prsent ProductForm when
@@ -28,57 +22,9 @@ export function ProductUpsertDialog(props: {
   product?: Product;
 }) {
 
-  const [err, setErr] = useState('');
-
-  const handleSubmit = (
-    values: ProductFormVal,
-    helpers: FormikHelpers<ProductFormVal>
-  ) => {
-    helpers.setSubmitting(true);
-    setErr('');
-
-    // Update
-    if (props.product) {
-      const params = buildUpdateProductParams(values);
-
-      updateFtcProduct(props.product.id, params, {
-        live: props.product.liveMode,
-        token: props.passport.token,
-      })
-        .then((prod) => {
-          helpers.setSubmitting(false);
-          toast.success('Update succeeded');
-          props.onUpserted(prod);
-        })
-        .catch((err: ResponseError) => {
-          helpers.setSubmitting(false);
-          setErr(err.message);
-        });
-    } else {
-      // Create
-      const params = buildNewProductParams(values, props.passport.userName);
-
-      createFtcProduct(params, {
-        live: props.live,
-        token: props.passport.token
-      })
-        .then((prod) => {
-          helpers.setSubmitting(false);
-          console.log(prod);
-          toast.success('Product created');
-          props.onUpserted(prod);
-        })
-        .catch((err: ResponseError) => {
-          helpers.setSubmitting(false);
-          toast.error(err.message);
-          if (err.statusCode === 422) {
-            helpers.setErrors(err.toFormFields);
-            return;
-          }
-          setErr(err.message);
-        });
-    }
-  };
+  const {
+    updateProduct,
+  } = useProductUpsert();
 
   return (
     <Modal show={props.show} fullscreen={true} onHide={props.onHide}>
@@ -89,17 +35,17 @@ export function ProductUpsertDialog(props: {
         <ModeBadge live={props.live} />
       </Modal.Header>
       <Modal.Body>
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-md-8 col-lg-6">
-              <ProductForm
-                onSubmit={handleSubmit}
-                errMsg={err}
-                product={props.product}
-              />
-            </div>
-          </div>
-        </div>
+        <SingleCenterCol>
+          <ProductForm
+            onSubmit={updateProduct({
+              passport: props.passport,
+              live: props.live,
+              onSaved: props.onUpserted,
+              product: props.product,
+            })}
+            product={props.product}
+          />
+        </SingleCenterCol>
       </Modal.Body>
     </Modal>
   );
