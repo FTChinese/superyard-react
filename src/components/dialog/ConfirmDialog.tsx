fcf4f5a8c45b9f5bcc80f5d14928f8442dbec717
weiguo.ni <neefrankie@gmail.com>
2023-06-02 12:42:26 +0800

    a layout dialog for confirm

---
+++ src/components/dialog/ConfirmDialog.tsx
@@ -0,0 +1,42 @@
+import Modal from 'react-bootstrap/Modal';
+import { ModeBadge } from '../text/Badge';
+import { LoadButton } from '../buttons/Button';
+
+export function ConfirmDialog(
+  props: {
+    title: string;
+    body: string;
+    show: boolean;
+    onHide: () => void;
+    live: boolean;
+    onConfirm: () => void;
+    progress: boolean;
+  }
+) {
+  return (
+    <Modal
+      show={props.show}
+      onHide={props.onHide}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title className='me-3'>
+          {props.title}
+        </Modal.Title>
+        <ModeBadge live={props.live} />
+      </Modal.Header>
+
+      <Modal.Body>
+        {props.body}
+      </Modal.Body>
+
+      <Modal.Footer>
+        <LoadButton
+          text='Yes'
+          disabled={props.progress}
+          onClick={props.onConfirm}
+          progress={props.progress}
+        />
+      </Modal.Footer>
+    </Modal>
+  )
+}
