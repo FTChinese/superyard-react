8bafc4f3e6865ee46619db6f0848978f3615700a
weiguo.ni <neefrankie@gmail.com>
2023-06-02 12:53:34 +0800

    refactor layout

--- src/pages/paywall/FtcPricepage.tsx
+++ src/pages/paywall/FtcPricepage.tsx
@@ -2,18 +2,20 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAuth } from '../../components/hooks/useAuth';
 import { useLiveMode } from '../../components/hooks/useLiveMode';
-import { useProgress } from '../../components/hooks/useProgress';
 import { Unauthorized } from '../../components/middleware/Unauthorized';
 import { CMSPassport } from '../../data/cms-account';
 import { isOneTime } from '../../data/enum';
-import { toast } from 'react-toastify';
-import { FtcPriceScreen } from '../../features/ftcprice/FtcPriceScreen';
+import { OfferListSection, OfferItem } from '../../features/ftcprice/OfferList';
 import { useFtcPrice } from '../../features/ftcprice/useFtcPrice';
-import { NewOfferDialog } from '../../features/ftcprice/NewOfferDialog';
-import { DropOfferDialog } from '../../features/ftcprice/DropOfferDialog';
+import { NewOfferDialog } from '../../features/ftcprice/OfferDialog';
+import { DropOfferDialog } from '../../features/ftcprice/OfferDialog';
 import { Discount } from '../../data/ftc-price';
-import { PriceFormDialog } from '../../features/ftcprice/PriceFormDialog';
+import { FtcPriceActiveDialog, FtcPriceArchiveDialog, FtcPriceFormDialog } from '../../features/ftcprice/FtcPriceDialog';
 import { ErrorText } from '../../components/text/ErrorText';
+import { PriceForm } from '../../features/ftcprice/FtcPriceForm';
+import { useUpsertFtcPrice } from '../../features/ftcprice/useUpsertFtcPrice';
+import { ReqConfig } from '../../http/ReqConfig';
+import { FtcPriceMenu, FtcPriceSection } from '../../features/ftcprice/FtcPriceCard';
 
 /**
  * FtcPricePage is a page to show a single FTC price.
@@ -49,29 +51,34 @@ function PricePageScreen(
   }
 ) {
 
+  const config: ReqConfig = {
+    live: props.live,
+    token: props.passport.token,
+  };
+
   const [showPriceForm, setShowPriceForm] = useState(false);
+  const [showPriceActivate, setShowPriceActivate] = useState(false);
+  const [showPriceArchive, setShowPriceArchive] = useState(false);
+
   const [showOfferForm, setShowOfferForm] = useState(false);
 
-  const [alertDropOffer, setAlertDropOffer] = useState<Discount>();
-
-  const { progress } = useProgress();
+  const [deleteTargetOffer, setDeleteTargetOffer] = useState<Discount>();
 
   const {
     price,
     loadPrice,
-    activatePrice,
-    archivePrice,
     onPriceUpdated,
     onNewOffer,
     refreshOffers,
   } = useFtcPrice();
 
+  const {
+    updatePrice,
+  } = useUpsertFtcPrice();
+
   // Load ftc price upon mount.
   useEffect(() => {
-    loadPrice(props.priceId, {
-      live: props.live,
-      token: props.passport.token,
-    });
+    loadPrice(props.priceId, config);
   }, [props.live]);
 
   if (!price) {
@@ -80,50 +87,89 @@ function PricePageScreen(
 
   return (
     <>
-      <FtcPriceScreen
+      <FtcPriceSection
         price={price}
-        progress={progress}
-        onEdit={() => {
-          setShowPriceForm(true);
-        }}
-        onActivate={() => {
-          activatePrice(price.id, {
-            live: props.live,
-            token: props.passport.token,
-          });
-        }}
-        onArchive={() => {
-          archivePrice(price.id, {
-            live: props.live,
-            token: props.passport.token,
-          })
-        }}
-        onNewOffer={() => {
-          if (isOneTime(price.kind)) {
-            toast.error('Introductory price cannot have coupons');
-            return;
-          }
-          setShowOfferForm(true);
-        }}
-        onRefreshOffers={() => {
-          refreshOffers(price.id, {
-            live: props.live,
-            token: props.passport.token,
-          })
-        }}
-        onDeleteOffer={(o) => {
-          setAlertDropOffer(o);
-        }}
+        menu={
+          <FtcPriceMenu
+            active={price.active}
+            archived={price.archived}
+            progress={false}
+            onEdit={() => {
+              setShowPriceForm(true);
+            }}
+            onActivate={() => {
+              setShowPriceActivate(true);
+            }}
+            onArchive={() => {
+              setShowPriceArchive(true);
+            }}
+          />
+        }
       />
 
-      <PriceFormDialog
-        passport={props.passport}
-        live={props.live}
+      <FtcPriceFormDialog
         show={showPriceForm}
         onHide={() => setShowPriceForm(false)}
-        onUpserted={onPriceUpdated}
-        price={price}
+        live={props.live}
+        form={
+          <PriceForm
+            onSubmit={
+              updatePrice(
+                price,
+                config,
+                onPriceUpdated,
+              )
+            }
+            price={price}
+          />
+        }
+        isCreate={false}
+        tier={price.tier}
       />
+
+      <FtcPriceActiveDialog
+        show={showPriceActivate}
+        onHide={() => setShowPriceActivate(false)}
+        config={config}
+        price={price}
+        onSaved={onPriceUpdated}
+      />
+
+      <FtcPriceArchiveDialog
+        show={showPriceArchive}
+        onHide={() => setShowPriceArchive(false)}
+        config={config}
+        price={price}
+        onSaved={onPriceUpdated}
+      />
+
+      {
+        isOneTime(price.kind) ?
+          <div className='text-danger'>
+          Introductory price has no discount.
+          </div> :
+          <OfferListSection
+            progress={false}
+            onNewOffer={() => {
+              setShowOfferForm(true);
+            }}
+            onRefresh={() => {
+              refreshOffers(price.id, {
+                live: props.live,
+                token: props.passport.token,
+              })
+            }}
+            items={
+              price.offers.map((offer) => (
+                <OfferItem
+                  offer={offer}
+                  onDelete={setDeleteTargetOffer}
+                />
+              ))
+            }
+          />
+      }
+
       <NewOfferDialog
         passport={props.passport}
         live={props.live}
@@ -132,21 +178,19 @@ function PricePageScreen(
         onHide={() => {
           setShowOfferForm(false);
         }}
-        onCreated={onNewOffer}
+        onSaved={onNewOffer}
       />
 
       {
-        alertDropOffer &&
+        deleteTargetOffer &&
         <DropOfferDialog
-          passport={props.passport}
-          live={props.live}
-          offer={alertDropOffer}
-          show={!!alertDropOffer}
-          onHide={() => setAlertDropOffer(undefined)}
+          config={config}
+          offer={deleteTargetOffer}
+          show={!!deleteTargetOffer}
+          onHide={() => setDeleteTargetOffer(undefined)}
           onDropped={onPriceUpdated}
         />
       }
-
     </>
   );
 }
